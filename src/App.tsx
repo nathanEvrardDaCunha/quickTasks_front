487a7211fbd7bdadf57ecc03ef70f9ab72b39908
nathanEvrardDaCunha <nathan.evrarddacunha@proton.me>
2025-05-17 17:51:37 +0200

    feat(core): add simple query to connect to backend default api route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // In index.html:
 // TO-DO: Add all generic meta tag.
@@ -10,14 +11,22 @@ import Register from './pages/Register';
 // TO-DO: Add all Twitter meta tag.
 // TO-DO: Add website icon.
 
+const queryClient = new QueryClient();
+
 const router = createBrowserRouter([
     { path: '/', element: <Home /> },
     { path: '/login', element: <Login /> },
     { path: '/register', element: <Register /> },
 ]);
 
+// TO-CONSIDER: Add react query dev tools ?
+
 function App() {
-    return <RouterProvider router={router} />;
+    return (
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
+    );
 }
 
 export default App;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,76 +1,98 @@
+import { useQuery } from '@tanstack/react-query';
+
 function Login() {
-    function handleAction(formData: FormData) {
-        for (const [key, value] of formData.entries()) {
-            console.log(key, value);
-        }
+    const { isLoading, error, data } = useQuery({
+        queryKey: ['defaultApiRoute'],
+        queryFn: async () => {
+            const response = await fetch('http://localhost:5003/api');
+            return await response.json();
+        },
+    });
+
+    if (error) {
+        return <h1>{error.message}</h1>;
     }
 
-    return (
-        <>
-            <header>
-                <h2>Website Header</h2>
-            </header>
-            <main>
-                <h1>Login Page</h1>
+    if (isLoading) {
+        return <h1>Loading...</h1>;
+    }
 
-                <form action={handleAction}>
-                    <fieldset>
-                        <legend>User Sign-In</legend>
+    // What if there are no data ? => error
+    return <em>{data.message}</em>;
 
-                        <label htmlFor="sign-in-email">Email Address</label>
-                        <input
-                            type="email"
-                            name="sign-in-email"
-                            id="sign-in-email"
-                            required={true}
-                        />
+    // function handleAction(formData: FormData) {
+    //     for (const [key, value] of formData.entries()) {
+    //         console.log(key, value);
+    //     }
+    // }
 
-                        {/* <input
-                            type="email"
-                            name="sign-up-email"
-                            id="sign-up-email"
-                            required={true}
-                            value={undefined}
-                            onChange={undefined}
-                            minLength={undefined}
-                            maxLength={undefined}
-                            disabled={false}
-                            placeholder=""
-                            pattern={undefined}
-                        /> */}
+    // return (
+    //     <>
+    //         <header>
+    //             <h2>Website Header</h2>
+    //         </header>
 
-                        <label htmlFor="sign-in-password">Password</label>
-                        <input
-                            type="password"
-                            name="sign-in-password"
-                            id="sign-in-password"
-                            required={true}
-                        />
+    //         <main>
+    //             <h1>Login Page</h1>
 
-                        {/* <input
-                            type="password"
-                            name="sign-up-password"
-                            id="sign-up-password"
-                            required={true}
-                            value={undefined}
-                            onChange={undefined}
-                            minLength={undefined}
-                            maxLength={undefined}
-                            disabled={false}
-                            placeholder=""
-                            pattern={undefined}
-                        /> */}
+    //             <form action={handleAction}>
+    //                 <fieldset>
+    //                     <legend>User Sign-In</legend>
 
-                        <button type="submit">Submit</button>
-                        <button type="reset">Reset</button>
-                    </fieldset>
-                </form>
-            </main>
-            <footer>
-                <h2>Website Footer</h2>
-            </footer>
-        </>
-    );
+    //                     <label htmlFor="sign-in-email">Email Address</label>
+    //                     <input
+    //                         type="email"
+    //                         name="sign-in-email"
+    //                         id="sign-in-email"
+    //                         required={true}
+    //                     />
+
+    //                     {/* <input
+    //                         type="email"
+    //                         name="sign-up-email"
+    //                         id="sign-up-email"
+    //                         required={true}
+    //                         value={undefined}
+    //                         onChange={undefined}
+    //                         minLength={undefined}
+    //                         maxLength={undefined}
+    //                         disabled={false}
+    //                         placeholder=""
+    //                         pattern={undefined}
+    //                     /> */}
+
+    //                     <label htmlFor="sign-in-password">Password</label>
+    //                     <input
+    //                         type="password"
+    //                         name="sign-in-password"
+    //                         id="sign-in-password"
+    //                         required={true}
+    //                     />
+
+    //                     {/* <input
+    //                         type="password"
+    //                         name="sign-up-password"
+    //                         id="sign-up-password"
+    //                         required={true}
+    //                         value={undefined}
+    //                         onChange={undefined}
+    //                         minLength={undefined}
+    //                         maxLength={undefined}
+    //                         disabled={false}
+    //                         placeholder=""
+    //                         pattern={undefined}
+    //                     /> */}
+
+    //                     <button type="submit">Submit</button>
+    //                     <button type="reset">Reset</button>
+    //                 </fieldset>
+    //             </form>
+    //         </main>
+    //         <footer>
+    //             <h2>Website Footer</h2>
+    //         </footer>
+    //     </>
+    // );
 }
 
 export default Login;

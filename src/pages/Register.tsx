8c3f5c2108a322b16339fc8e41862981a13ef82c
nathanEvrardDaCunha <nathan.evrarddacunha@proton.me>
2025-05-18 21:40:28 +0200

    feat(auth): implement entirely functional register logic

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,10 +1,112 @@
+import { useMutation } from '@tanstack/react-query';
+import { useState } from 'react';
+
+// Send it into a dedicated type file ?
+type User = {
+    username: string;
+    email: string;
+    password: string;
+};
+
+type APIError = {
+    name: string;
+    cause: string;
+    stack: string;
+};
+
+// Should remove the status from here and the server.
+type APISuccess = {
+    status: string;
+    message: string;
+};
+
 function Register() {
-    // TO-CONSIDER: Is it necessary to add constraint to input when the server is already doing this job for us ?
+    const [formData, setFormData] = useState<User>({
+        username: '',
+        email: '',
+        password: '',
+    });
+
+    // Hook ('useSomething' don't trigger at every rerunder it seem)
+    // What about normal function ? Create one and see for yourself !
+
+    const mutation = useMutation({
+        mutationKey: ['registerUser'],
+        mutationFn: async (user: User) => {
+            const result = await fetch(
+                `http://localhost:5003/api/auth/register`,
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(user),
+                }
+            );
+
+            // Is this part really mandatory ?
+            // When I delete it, the onError seem to doesn't work, but I'm not sure if it's really mandatory.
+            if (!result.ok) {
+                const errorData: APIError = await result.json();
+                throw errorData;
+            }
+
+            return await result.json();
+        },
+        onError: (error: APIError) => {
+            console.error(`${error.name}: ${error.cause}`);
+        },
+        onSuccess: () => {
+            handleReset();
+        },
+    });
 
     function handleAction(formData: FormData) {
-        for (const [key, value] of formData.entries()) {
-            console.log(key, value);
+        // DESIGN: The client is responsible for sending every field while the server is
+        // responsible for validating the content of them and sending error if necessary
+        const username = formData.get('username');
+        const email = formData.get('email');
+        const password = formData.get('password');
+
+        if (!username) {
+            throw new Error(
+                `Couldn't find the username property of the form !`
+            );
         }
+
+        if (!email) {
+            throw new Error(`Couldn't find the email property of the form !`);
+        }
+
+        if (!password) {
+            throw new Error(
+                `Couldn't find the password property of the form !`
+            );
+        }
+
+        const user: User = {
+            username: username as string,
+            email: email as string,
+            password: password as string,
+        };
+
+        mutation.mutate(user);
+    }
+
+    function handleOnChange(event: any) {
+        const { name, value } = event.target;
+        setFormData((previous) => ({
+            ...previous,
+            [name]: value,
+        }));
+    }
+
+    function handleReset() {
+        setFormData({
+            username: '',
+            email: '',
+            password: '',
+        });
     }
 
     return (
@@ -15,79 +117,56 @@ function Register() {
             <main>
                 <h1>Register Page</h1>
 
+                {mutation.isSuccess && (
+                    <h2>Success: {(mutation.data as APISuccess).message}</h2>
+                )}
+
+                {mutation.error && (
+                    <h2>Error: {(mutation.error as APIError).cause} </h2>
+                )}
+
+                {mutation.isPending && <h2>Action processing...</h2>}
+
                 <form action={handleAction}>
                     <fieldset>
                         <legend>User Sign-Up</legend>
 
-                        <label htmlFor="sign-up-username">Username</label>
+                        <label htmlFor="username">Username</label>
                         <input
                             type="text"
-                            name="sign-up-button"
-                            id="sign-up-button"
-                            required={true}
+                            name="username"
+                            id="username"
+                            value={formData.username}
+                            onChange={handleOnChange}
+                            required
                         />
 
-                        {/* <input
-                            type="text"
-                            name="sign-up-button"
-                            id="sign-up-button"
-                            required={true}
-                            value={undefined}
-                            onChange={undefined}
-                            minLength={undefined}
-                            maxLength={undefined}
-                            spellCheck={false}
-                            disabled={false}
-                            placeholder=""
-                            pattern={undefined}
-                        /> */}
-
-                        <label htmlFor="sign-up-email">Email Address</label>
+                        <label htmlFor="email">Email Address</label>
                         <input
                             type="email"
-                            name="sign-up-email"
-                            id="sign-up-email"
-                            required={true}
+                            name="email"
+                            id="email"
+                            value={formData.email}
+                            onChange={handleOnChange}
+                            required
                         />
 
-                        {/* <input
-                            type="email"
-                            name="sign-up-email"
-                            id="sign-up-email"
-                            required={true}
-                            value={undefined}
-                            onChange={undefined}
-                            minLength={undefined}
-                            maxLength={undefined}
-                            disabled={false}
-                            placeholder=""
-                            pattern={undefined}
-                        /> */}
-
-                        <label htmlFor="sign-up-password">Password</label>
+                        <label htmlFor="password">Password</label>
                         <input
                             type="password"
-                            name="sign-up-password"
-                            id="sign-up-password"
-                            required={true}
+                            name="password"
+                            id="password"
+                            value={formData.password}
+                            onChange={handleOnChange}
+                            required
                         />
 
-                        {/* <input
-                            type="password"
-                            name="sign-up-password"
-                            id="sign-up-password"
-                            required={true}
-                            value={undefined}
-                            onChange={undefined}
-                            minLength={undefined}
-                            maxLength={undefined}
-                            disabled={false}
-                            placeholder=""
-                            pattern={undefined}
-                        /> */}
-
-                        <button type="submit">Submit</button>
-                        <button type="reset">Reset</button>
+                        <button type="submit" disabled={mutation.isPending}>
+                            {mutation.isPending ? 'Submitting...' : 'Submit'}
+                        </button>
+                        <button type="button" onClick={handleReset}>
+                            Reset
+                        </button>
                     </fieldset>
                 </form>
             </main>

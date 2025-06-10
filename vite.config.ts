26b358dac45dfb06a0e0b9e48a2a039045f4e6c5
nathanEvrardDaCunha <nathan.evrarddacunha@proton.me>
2025-06-10 13:50:29 +0200

    build(production): add file for production deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    build: {
+        sourcemap: false,
+        minify: 'terser',
+        chunkSizeWarningLimit: 1000,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['react', 'react-dom', 'react-router-dom'],
+                    query: ['@tanstack/react-query'],
+                },
+            },
+        },
+    },
+    server: {
+        port: 5173,
+        host: true,
+    },
+});
